08303ae41271e18c851d2f7c20b9cfee64013e6a
Xuanyu Lyu <xuanyu.lyu@colorado.edu>
2024-05-29 13:37:32 -0600

    Update .sh

--- BiSEMPGS.sh
+++ BiSEMPGS.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#SBATCH --qos=blanca -preemptable
-#SBATCH --time=12:00:00
+#SBATCH --qos=preemptable
+#SBATCH --time=48:00:00
 #SBATCH --ntasks=2
 #SBATCH --mem=16gb
 #SBATCH --array=1-10
@@ -9,10 +9,11 @@
 #SBATCH --exclude bmem-rico1,bnode010[1-5]
 #SBATCH -o %x.out%A
 #SBATCH -e %x.err%A
+
 source /curc/sw/anaconda3/R-4.3.3
 conda activate /projects/lessem/software/anaconda/envs/R-4.3.3
 
 # Index to run different parts of the script, e.g., processing different chromosomes; Here is the different simulation conditions I want to run
 SIM=${SLURM_ARRAY_TASK_ID}
 
-Rscript rc_run2simulate.R ${CHR} 
+Rscript rc_run2simulate.R ${SIM} 
