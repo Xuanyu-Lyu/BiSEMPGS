17099816e85a073ea4ff93e3ff0a446e9c61d062
Xuanyu Lyu <xuanyu.lyu@colorado.edu>
2024-06-06 08:51:40 -0600

    correct .sh

--- BiSEMPGS.sh
+++ BiSEMPGS.sh
@@ -3,7 +3,7 @@
 #SBATCH --time=16:00:00
 #SBATCH --ntasks=1
 #SBATCH --mem=80gb
-#SBATCH --array=1:50%10
+#SBATCH --array=1-50%10
 #SBATCH -J BiSEMPGS
 #SBATCH --chdir /projects/xuly4739/R-Projects/BiSEMPGS/BiSEMPGS
 #SBATCH --exclude bmem-rico1,bnode010[1-5]
