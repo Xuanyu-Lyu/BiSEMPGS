ce513ebeb4863c756c1ab73b64f7b0fce8c72fce
Xuanyu Lyu <xuanyu.lyu@colorado.edu>
2024-06-20 01:07:35 -0600

    change the constraint to adelta

--- BiSEMPGS_fit_rc.R
+++ BiSEMPGS_fit_rc.R
@@ -6,17 +6,17 @@ source("/projects/xuly4739/R-Projects/BiSEMPGS/BiSEMPGS/BiSEMPGS_fitOpenMx.R")
 conditionNames <- c("Full_Model", "MeasurePgs30", "MeasurePgs10", "MeasurePgsFully", 
 "f11-decrease", "f12-decrease", "f11.12.21.22-decrease", "am11-decrease", "am12-decrease", "am11.12.21.22-decrease", "Full_Model_.5latent")
 # the path to the dir with all txt files
-data_path <- c( paste0("/projects/xuly4739/R-Projects/BiSEMPGS/BiSEMPGS/Data/", conditionNames[1], "n/fam16000"),
-                paste0("/projects/xuly4739/R-Projects/BiSEMPGS/BiSEMPGS/Data/", conditionNames[1], "/nfam48000"),
-                paste0("/projects/xuly4739/R-Projects/BiSEMPGS/BiSEMPGS/Data/", conditionNames[1], "/nfam32000"),
-                paste0("/projects/xuly4739/R-Projects/BiSEMPGS/BiSEMPGS/Data/", conditionNames[1], "/nfam64000"))
+data_path <- c( paste0("/projects/xuly4739/R-Projects/BiSEMPGS/BiSEMPGS/Data/", conditionNames[11], "/nfam16000"),
+                paste0("/projects/xuly4739/R-Projects/BiSEMPGS/BiSEMPGS/Data/", conditionNames[11], "/nfam48000"),
+                paste0("/projects/xuly4739/R-Projects/BiSEMPGS/BiSEMPGS/Data/", conditionNames[11], "/nfam32000"),
+                paste0("/projects/xuly4739/R-Projects/BiSEMPGS/BiSEMPGS/Data/", conditionNames[11], "/nfam64000"))
 # the path to the dir to save the results
-save_path <- paste0("/projects/xuly4739/R-Projects/BiSEMPGS/BiSEMPGS/Analysis/", conditionNames[1])
+save_path <- paste0("/projects/xuly4739/R-Projects/BiSEMPGS/BiSEMPGS/Analysis/", conditionNames[11])
 
 data_pattern <- c("_16000.txt", "_48000.txt", "_32000.txt", "_64000.txt")
 save_pattern <- c("_16000","_48000", "_32000", "_64000")
 model_type <- "m2"
-mxSetup <- "_.05lb_smallerTol_newSetup"
+mxSetup <- "_.05lb_smallerTol_newSetup_adelta"
 for (j in 1:4){
     # a list to save all the summary data
     summary_list <- list()
@@ -25,7 +25,7 @@ for (j in 1:4){
     l_files <- list.files(data_path[j], pattern = data_pattern[j])
 
     # fit the model for each data file
-    for (i in 1:length(l_files){
+    for (i in 1:length(l_files)){
         # fit the model
         fit <- fitBiSEMPGS_m2(paste0(data_path[j], "/", l_files[i]))
         # save the summary
