5cabd1c605c87d51ff4793d1e73cc7bb7537a243
Xuanyu Lyu <xuanyu.lyu@colorado.edu>
2024-08-01 16:48:50 +0800

    Only fit 64k to check results

--- BiSEMPGS_fit_rc.R
+++ BiSEMPGS_fit_rc.R
@@ -16,8 +16,8 @@ save_path <- paste0("/projects/xuly4739/R-Projects/BiSEMPGS/BiSEMPGS/Analysis/",
 data_pattern <- c( "_48000.txt", "_32000.txt", "_64000.txt")
 save_pattern <- c("_48000", "_32000", "_64000")
 model_type <- "m2"
-mxSetup <- "_.05lb_smallerTol_newSetup_fixedArg"
-for (j in 1:3){
+mxSetup <- "_.05lb_smallerTol_newSetup_fixedArg_closerh"
+for (j in 3:3){
     # a list to save all the summary data
     summary_list <- list()
 
