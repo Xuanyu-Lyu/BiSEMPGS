4f1b7eeefbfbb36e515c14df4ab42c757e2c61af
Xuanyu Lyu <xuanyu.lyu@colorado.edu>
2024-10-01 15:11:59 -0600

    clean the new simulated files

--- BiSEMPGS_2txt.sh
+++ BiSEMPGS_2txt.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 #SBATCH --qos=preemptable
 #SBATCH --time=8:00:00
-#SBATCH --ntasks=2
+#SBATCH --ntasks=4
 #SBATCH --mem=40gb
 #SBATCH -J BiSEMPGS_dataprep
 #SBATCH --chdir /projects/xuly4739/R-Projects/BiSEMPGS/BiSEMPGS
-#SBATCH --exclude bmem-rico1,bnode010[1-5]
+#SBATCH --exclude bmem-rico1
 #SBATCH -o %x.out%A
 #SBATCH -e %x.err%A
 
 source /curc/sw/anaconda3/latest
-conda activate /projects/lessem/software/anaconda/envs/R-4.4.0
+conda activate /projects/lessem/software/anaconda/envs/R-latest
 
 # Index to run different parts of the script, e.g., processing different chromosomes; Here is the different simulation conditions I want to run
 #SIM=${SLURM_ARRAY_TASK_ID}
 
-#Rscript BiSEMPGS_dataprep_rc.R
+Rscript BiSEMPGS_dataprep_rc.R
 Rscript BiSEMPGS_mergetxt.R
