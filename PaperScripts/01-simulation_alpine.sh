4a4561a103b2dd64c0f3c3f75ec2e1d68b434264
Xuanyu Lyu <xuanyu.lyu@colorado.edu>
2024-11-13 20:38:25 -0700

    update the alpine job script

---
+++ PaperScripts/01-simulation_alpine.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --partition=amem
+#SBATCH --qos=mem
+#SBATCH --time=24:00:00
+#SBATCH --ntasks=1
+#SBATCH --mem=300gb
+#SBATCH --array=1-1%1
+#SBATCH -J BiSEMPGS_simulation_new
+#SBATCH --chdir /projects/xuly4739/R-Projects/BiSEMPGS/BiSEMPGS
+#SBATCH -o %x.out%A
+#SBATCH -e %x.err%A
+
+source /curc/sw/anaconda3/latest
+conda activate /projects/lessem/software/anaconda/envs/R-latest
+
+# Index to run different parts of the script, e.g., processing different chromosomes; Here is the different simulation conditions I want to run
+SIM=${SLURM_ARRAY_TASK_ID}
+
+Rscript PaperScripts/01-run2simulate.R ${SIM} 
